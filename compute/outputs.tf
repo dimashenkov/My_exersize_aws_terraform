96f52e2d2d21f4c6f6845b5c127eb1b1874b6619
Dimitar Shenkov <dimitar.shenkov@ticketmaster.com>
2019-03-01 11:31:17 +0200

    Route53

--- compute/outputs.tf
+++ compute/outputs.tf
@@ -3,6 +3,8 @@ output "wp_dev_public_ip" {
 	value = "${aws_instance.wp_dev.public_ip}"
 }
 
+
+
 output "wp_db_address" {
 	value = "${aws_db_instance.wp_db.address}"
 }
--- storage/outputs.tf
+++ storage/outputs.tf
@@ -2,3 +2,7 @@
 output "s3code" {
   value = "${aws_s3_bucket.code.bucket}"
 }
+
+output "bucketname" {
+  value = "${aws_s3_bucket.code.id}"
+}
