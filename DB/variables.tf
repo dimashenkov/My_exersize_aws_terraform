23877acf66e2c156ca903c6d0c537f77d0584b3f
Dimitar Shenkov <dimitar.shenkov@ticketmaster.com>
2019-03-01 14:11:04 +0200

    DB

--- DB/variables.tf
+++ DB/variables.tf
@@ -3,6 +3,7 @@ variable "db_instance_class" {}
 variable "dbname" {}
 variable "dbuser" {}
 variable "dbpassword" {}
+variable "domain_name" {}
 
 variable "db_subnet_group_name" {}
 variable "wp_rds_security_group_ids" {}
