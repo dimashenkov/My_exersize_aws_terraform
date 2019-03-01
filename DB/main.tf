3f1c08369ab2751ce3bcacbe9852952c1dc1216d
Dimitar Shenkov <dimitar.shenkov@ticketmaster.com>
2019-03-01 13:19:24 +0200

    Route53

---
+++ DB/main.tf
@@ -0,0 +1,15 @@
+#--------------DB/main.tf-------------------
+
+resource "aws_db_instance" "wp_db" {
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.7.23"
+  instance_class         = "${var.db_instance_class}"
+  name                   = "${var.dbname}"
+  username               = "${var.dbuser}"
+  password               = "${var.dbpassword}"
+
+  db_subnet_group_name   = "${var.db_subnet_group_name}"
+  vpc_security_group_ids = ["${var.wp_rds_security_group_ids}"]
+  skip_final_snapshot    = true             #bez taq opciq nemojesh da destroynesh
+}
---
+++ DB/variables.tf
@@ -0,0 +1,8 @@
+#-------------DB/variables.tf--------------
+variable "db_instance_class" {}
+variable "dbname" {}
+variable "dbuser" {}
+variable "dbpassword" {}
+
+variable "db_subnet_group_name" {}
+variable "wp_rds_security_group_ids" {}
